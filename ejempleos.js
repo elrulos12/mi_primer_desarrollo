c8ae244cc89a818b0b6fff189661e1494f96c703
elrulos12 <itaaguilaredizon@gmail.com>
2024-10-24 12:15:24 -0500

    el primer commit

---
+++ ejempleos.js
@@ -0,0 +1,13 @@
+//nunca declarar una variable con var
+let a = {
+    nombre: "pedro",
+    edad: 13,
+    genero: "m",
+};
+let b = [1,2,3,4,5];
+let c = 45;
+let d = "hola";
+let ed = 'hola';
+
+console. log(ed); // es para imprimir el resultado de consola
+console. log('el nombre que esta guardado en a es :'+ a.nombre);
